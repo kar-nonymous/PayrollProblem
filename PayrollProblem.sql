6ba4895e3a4438a0792b972ef36cffc0777be886
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-01 21:07:41 +0530

    [Kartikeya] UC added to create a table

--- PayrollProblem.sql
+++ PayrollProblem.sql
@@ -1,4 +1,10 @@
-/*UC1: 
-Database created with payroll_service name*/
-create database payroll_service;
+/* UC 2 : 
+Employee payroll table is created*/
 use payroll_service;
+/* Creating a table */
+create table employee_payroll
+(EmpID int not null identity(1,1) primary key,
+EmpName varchar(150) not null,
+Salary float not null,
+StartDate date not null
+);
