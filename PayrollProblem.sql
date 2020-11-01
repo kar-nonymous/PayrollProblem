e531f0f6157e3fcc98000856ef7a6c1a9c599ba4
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-01 21:38:47 +0530

    [Kartikeya] UC added to find sum, average, min, max and count of salary

--- PayrollProblem.sql
+++ PayrollProblem.sql
@@ -1,5 +1,5 @@
-/* UC 6 :
-Retrive data from the table with a given condition*/
+/* UC 7 :
+Find sum, average, min, count and max of salary*/
 use payroll_service;
 /* Creating a table */
 create table employee_payroll
@@ -22,3 +22,13 @@ select * from employee_payroll where StartDate between cast('2020-01-01' as date
 alter table employee_payroll add Gender char(1);
 /*Update table*/
 update employee_payroll set Gender='M' where EmpName ='Kartikeya' or EmpName ='Morgan';
+/*Sum of salary of male and female employees*/
+select Gender,sum(salary) as SalarySum from employee_payroll where Gender='M' or Gender='F' group by Gender;
+/*Average salary of male and female employees*/
+select Gender,avg(salary) as AvgSalary from employee_payroll where Gender='M' or Gender='F' group by Gender;
+/*Minimum salary*/
+select Gender,min(salary) as MinSalary from employee_payroll where Gender='M' or Gender='F' group by Gender;
+/*Maximum salary*/
+select Gender,max(salary) as MaxSalary from employee_payroll where Gender='M' or Gender='F' group by Gender;
+/*Employee count*/
+select Gender,count(salary) as EmpCount from employee_payroll where Gender='M' or Gender='F' group by Gender;
